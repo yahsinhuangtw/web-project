0efc40a182ecae510b657392e797c227cf9011c1
Yahsin Huang <yahsinhuangtw@gmail.com>
2024-02-08 20:11:00 +0800

    build todoListHTML

--- scripts/todo-list.js
+++ scripts/todo-list.js
@@ -1,9 +1,21 @@
-const todoList = [];//creates an array with no values inside.
+const todoList = ['make dinner', 'make coffee'];
+
+let todoListHTML = '';
+
+for (let i = 0; i < todoList.length; i++) {
+    const todo = todoList[i];
+    const html = `<p>${todo}</p>`;
+    todoListHTML += html;
+}
+console.log(todoListHTML);
+
+document.querySelector('.js-todo-list')
+    .innerHTML = todoListHTML;
 
 function addTodo() {
     const inputElement = document.querySelector('.js-name-input');
     const name = inputElement.value; // the value property represents the text in the textbox.
-    
+
     todoList.push(name);
     console.log(todoList);
 
